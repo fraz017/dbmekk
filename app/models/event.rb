a94b70c1e685bbcbbd65356c8e3ec5d42c16fef6
Fraz Ahsan <fraz.ahsan17@gmail.com>
2018-04-09 15:29:50 +0500

    Email commented

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,14 +3,14 @@ class Event < ApplicationRecord
   # validates :email, presence: true
   validates :phone_number, presence: true
   validates :license_no, presence: true
-  after_create :notify_admin
+  # after_create :notify_admin
   has_one :bill
   validates_format_of :email, :with => Devise::email_regexp, message: "Invalid Email"
   accepts_nested_attributes_for :bill, :allow_destroy => true
   attr_accessor :date_range
   scope :in_daterange, -> (start_date, end_date) { where('end > ? and start < ?', start_date, end_date) }
 
-  after_update :send_email
+  # after_update :send_email
 
   def send_email
     if booking_time_changed? && !email_sent?
